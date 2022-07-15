95778a3cad196f7e712f00c9cbb0eec28841d4cd
Vignesh E <evicky2002@gmail.com>
2022-07-15 23:40:07 +0530

    optimised code

--- app/src/main/java/com/vigneshelumalai/MainActivity.kt
+++ app/src/main/java/com/vigneshelumalai/MainActivity.kt
@@ -3,7 +3,6 @@ package com.vigneshelumalai
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import java.util.*
@@ -21,104 +20,93 @@ class MainActivity : AppCompatActivity() {
             clickDatePicker()
         }
     }
-    fun clickDatePicker(){
-        val myCalendar = Calendar.getInstance();
-        val year = myCalendar.get(Calendar.YEAR)
-        val month = myCalendar.get(Calendar.MONTH)
-        println("month: $month")
-        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
-        DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener{ view, selectedYear, selectedMonth, selectedDayOfMonth ->
+    private fun clickDatePicker(){
+        val myCalendar = Calendar.getInstance()
+        val currentYear = myCalendar.get(Calendar.YEAR)
+        val currentMonth = myCalendar.get(Calendar.MONTH)
+        val currentDay = myCalendar.get(Calendar.DAY_OF_MONTH)
+         val dpd = DatePickerDialog(this,
+            { _, selectedYear, selectedMonth, selectedDayOfMonth ->
                 val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
-                val isLeapyear = isLeapYear(year)
-                var totalDays = getTotalDays(year,selectedYear)
-                var remDays2 = 0
+                val isLeapYear = isLeapYear(currentYear)
+                var totalDays = getTotalDays(currentYear,selectedYear)
+                var selectedYearExcessDays = 0
                 for(i in 1 until selectedMonth+1){
                     println("here : $i")
                     when(i){
-                        1 -> remDays2+=31
-                        2 -> if(isLeapyear) remDays2+=29 else remDays2+=28
-                        3 -> remDays2+=31
-                        4 -> remDays2+=30
-                        5 -> remDays2+=31
-                        6 -> remDays2+=30
-                        7 -> remDays2+=31
-                        8 -> remDays2+=31
-                        9 -> remDays2+=30
-                        10 -> remDays2+=30
-                        11 -> remDays2+=30
-                        12 -> remDays2+=31
+                        1 -> selectedYearExcessDays+=31
+                        2 -> selectedYearExcessDays += if(isLeapYear) 29 else 28
+                        3 -> selectedYearExcessDays+=31
+                        4 -> selectedYearExcessDays+=30
+                        5 -> selectedYearExcessDays+=31
+                        6 -> selectedYearExcessDays+=30
+                        7 -> selectedYearExcessDays+=31
+                        8 -> selectedYearExcessDays+=31
+                        9 -> selectedYearExcessDays+=30
+                        10 -> selectedYearExcessDays+=30
+                        11 -> selectedYearExcessDays+=30
+                        12 -> selectedYearExcessDays+=31
                     }
                 }
-                remDays2 += selectedDayOfMonth
-                var remDays = 0
-                for(i in 1 until month+1){
+                selectedYearExcessDays += selectedDayOfMonth
+                var currentYearGonePastDays = 0
+                for(i in 1 until currentMonth+1){
                     println("here : $i")
                     when(i){
-                        1 -> remDays+=31
-                        2 -> if(isLeapyear) remDays+=29 else remDays+=28
-                        3 -> remDays+=31
-                        4 -> remDays+=30
-                        5 -> remDays+=31
-                        6 -> remDays+=30
-                        7 -> remDays+=31
-                        8 -> remDays+=31
-                        9 -> remDays+=30
-                        10 -> remDays+=30
-                        11 -> remDays+=30
-                        12 -> remDays+=31
+                        1 -> currentYearGonePastDays+=31
+                        2 -> currentYearGonePastDays += if(isLeapYear) 29 else 28
+                        3 -> currentYearGonePastDays+=31
+                        4 -> currentYearGonePastDays+=30
+                        5 -> currentYearGonePastDays+=31
+                        6 -> currentYearGonePastDays+=30
+                        7 -> currentYearGonePastDays+=31
+                        8 -> currentYearGonePastDays+=31
+                        9 -> currentYearGonePastDays+=30
+                        10 -> currentYearGonePastDays+=30
+                        11 -> currentYearGonePastDays+=30
+                        12 -> currentYearGonePastDays+=31
                     }
                 }
-                remDays+=day
-                if(isLeapyear){
-                    var temp = 366 - remDays
-                    totalDays = totalDays - temp
+                currentYearGonePastDays+=currentDay
+                totalDays = if(isLeapYear){
+                    val currentYearRemainingDays = 366 - currentYearGonePastDays
+                    totalDays - currentYearRemainingDays
                 }else{
-                    var temp = 365 - remDays
-                    totalDays = totalDays - temp
+                    val currentYearRemainingDays = 365 - currentYearGonePastDays
+                    totalDays - currentYearRemainingDays
 
                 }
-                totalDays-=remDays2
-                println("here : $remDays2")
-                println("here : $totalDays")
-                println("here : $remDays")
+                totalDays -= selectedYearExcessDays
                 val ageInMinutes = totalDays*24*60
-                tvAgeInMinutes?.setText(ageInMinutes.toString())
-                tvSelectedDate?.setText(selectedDate)
+                tvAgeInMinutes?.text = (ageInMinutes.toString())
+                tvSelectedDate?.text = (selectedDate)
 
             },
-            year,
-            month,
-            day
-            ).show()
+            currentYear,
+            currentMonth,
+            currentDay
+            )
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
     }
-    fun getTotalDays(currentYear: Int, bornYear: Int): Int {
+    private fun getTotalDays(currentYear: Int, bornYear: Int): Int {
         var totalDays = 0
         for(i in bornYear..currentYear){
-            var isLeapYear = isLeapYear(i)
-            if(isLeapYear){
-                totalDays+=366
+            val isLeapYear = isLeapYear(i)
+            totalDays += if(isLeapYear){
+                366
             }else{
-                totalDays+=365
+                365
             }
-
         }
         return totalDays
     }
-    fun isLeapYear(year: Int): Boolean{
-        var isLeapYear = false
-        if (year % 400 == 0) {
-            isLeapYear = true
-        }
-        else if (year % 100 == 0) {
-            isLeapYear = false
-        }
-        else if (year % 4 == 0) {
-            isLeapYear = true
-        }
-        else {
-            isLeapYear = false
-        }
+    private fun isLeapYear(year: Int): Boolean{
+        val isLeapYear = if (year % 400 == 0) {
+            true
+        } else if (year % 100 == 0) {
+            false
+        } else year % 4 == 0
         return isLeapYear
     }
 }
